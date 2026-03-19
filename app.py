cda64459e4c8506d6a9d9155f65fe0def1f05f71
Parvesh Kumar <dhiman.kumar1903@gmail.com>
2026-03-19 20:47:24 +0530

    first comit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "AIzaSyCmiIHlXygxN_YJOUMrJFL3eCmVtNc22dY"
+
+@app.route("/")
+def home():
+    return render_template("index.html", show_result=False)
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    user_url = request.form.get("url")
+
+    if not user_url:
+        return "Please enter a valid URL"
+
+    if not user_url.startswith("http"):
+        user_url = "https://" + user_url
+
+    api_url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={user_url}&strategy=desktop&category=performance&category=accessibility&category=best-practices&category=seo&key={API_KEY}"    
+    res = requests.get(api_url)
+    data = res.json()
+
+    if "lighthouseResult" not in data:
+        return "Error fetching data"
+
+    lighthouse = data["lighthouseResult"]["categories"]
+
+    performance = lighthouse.get("performance", {}).get("score", 0) * 100
+    seo = lighthouse.get("seo", {}).get("score", 0) * 100
+    accessibility = lighthouse.get("accessibility", {}).get("score", 0) * 100
+    best_practices = lighthouse.get("best-practices", {}).get("score", 0) * 100
+
+    overall = int((performance + seo + accessibility + best_practices) / 4)
+
+    print(lighthouse)
+
+    return render_template(
+        "index.html",
+        performance=performance,
+        seo=seo,
+        accessibility=accessibility,
+        best_practices=best_practices,
+        overall=overall,
+        show_result=True
+    )
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    return "Coming soon"
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=10000)
